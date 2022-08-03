90020a99eca93a5aae24f3c6998110adb3e85b0a
thomas <thomas77140@hotmail.com>
2022-08-03 03:21:53 +0200

    ajout de la classe items

--- front/javascript/index.js
+++ front/javascript/index.js
@@ -1,16 +1,14 @@
 // avoir le tableau des informations du site par l'api
 let urlApi = 'http://localhost:3000/api/products'
 
-let urlApiGetId = ' http://localhost:3000/api/products'
 const affichage = document.getElementById(`items`)
 
 fetch(urlApi).then((Response) =>
     Response.json().then((data) => {
         console.log(data);
         data.forEach((urlApi) => {
-            affichage.insertAdjacentHTML("afterbegin", `<section class="items" id="items"></section>`);
+            affichage.insertAdjacentHTML("afterbegin", `<section class="items" id="items">${}</section>`);
+            affichage.classList.add("items")
         });
     })
 )
-
-console.log(urlApiGetId)
